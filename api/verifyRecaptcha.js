3d107186437351e0a65a017eeee0a4a28459236f
Ivan Dos Santos <102613357+AIVICODE@users.noreply.github.com>
2024-12-30 20:09:04 -0300

    Credentials

--- api/verifyRecaptcha.js
+++ api/verifyRecaptcha.js
@@ -7,8 +7,16 @@ export default async function handler(req, res) {
       const { recaptcha_token, from_name, emailjs_email, message } = req.body; // Asegúrate de que los datos estén presentes
       console.log("Datos recibidos en la API:", { recaptcha_token, from_name, emailjs_email, message });
 
-      // Llamar a la función de verificación de reCAPTCHA
-      const result = await verifyCaptcha(recaptcha_token, "submit"); // Asegúrate de que la acción sea la correcta
+      // Cargar el archivo de credenciales usando la variable de entorno
+      const credentials = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+
+      // Instanciar el cliente de reCAPTCHA
+      const client = new RecaptchaEnterpriseServiceClient({
+        keyFilename: credentials, // Pasa la ruta del archivo JSON de las credenciales
+      });
+
+      // Llamar a la función de verificación de reCAPTCHA usando el token recibido
+      const result = await verifyCaptcha(recaptcha_token, "submit", client); // Pasa el cliente a la función de verificación
 
       if (result.success) {
         res.status(200).json({ success: true });
