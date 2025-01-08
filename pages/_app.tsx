988ce5b1245702ed5b67725ecf4059cf94e6c0a2
Ivan Dos Santos <102613357+AIVICODE@users.noreply.github.com>
2025-01-07 22:31:27 -0300

    Update _app.tsx
    Agrega cosa de SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,21 @@ import '../styles/global.css'; // Importa el archivo de estilos globales
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" type="image/svg+xml" href="/images/logo.svg" /> {/* Asegúrate de que la ruta sea relativa a la carpeta "public" */}
+        <meta name="author" content="Iván Dos Santos" />
+        <meta name="robots" content="index, follow" />
+        <meta name="keywords" content="Diseño web, Diseño web para PYMES, Tiendas online, Desarrollo web profesional" />
+        <meta name="description" content="Creamos sitios web profesionales para PYMES y emprendedores que desean destacar en el mundo digital. Contáctanos para más información." />
+        <title>Iván Dos Santos | Diseño Web Profesional</title> {/* Agrega un título */}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
